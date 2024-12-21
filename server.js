afefc44d2ccc87382ddd90068e8c59611c54c890
Phạm Hoàng Quân <109340804+Ttosok@users.noreply.github.com>
2024-12-21 07:54:30 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,48 +1,19 @@
-const app = require("express")()
-const http = require('http').createServer(app)
-const io = require("socket.io")(http)
-const {joinUser, removeUser} = require("./public/javascript/user")
+const express = require('express');
+const path = require('path');
 
-app.get("/", (req,res) =>{
-    res.sendFile(__dirname + "/public/html/index.html")
-})
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-let thisRoom = ""
-// listen to connection event from client
-/* io.on("connection", (socket) =>{
-    console.log("connected")
-    // listen to join room event
-    socket.on("join room", (data) =>{
-        // calling joinUser function from user.js
-        let newUser = joinUser(socket.id, data.username, data.roomname)
-        // calling event send data with sending data
-        socket.emit("send data", {
-            id: socket.id,
-            username: newUser.username,
-            roomname: newUser.roomname
-        })
-        thisRoom = newUser.roomname
-        console.log(newUser)
-        socket.join(newUser.roomname)
-    })
-    // listen to chat message event
-    socket.on("chat message", (data) =>{
-        io.to(thisRoom).emit("chat message", {
-            data: data,
-            id: socket.id
-        })
-    })
-    // listen to disconnect event
-    socket.on("disconnect", () => {
-        const user = removeUser(socket.id);
-        console.log(user);
-        if(user) {
-            console.log(user.username + ' has left');
-        }
-        console.log("disconnected");
-    });
-})
- */
-const PORT = process.env.PORT || 8000
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-http.listen(PORT, () => {console.log(`socket start at ${PORT}`)})
+// Route to serve the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
